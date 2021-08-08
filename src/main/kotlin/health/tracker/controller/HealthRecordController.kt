4b1f7b6842cb19a04132fee9bbcf07956b978b1b
Martin Fürstenau <martin.fuerstenau@freenet.de>
2021-08-08 18:04:53 +0200

    from Flux to kotlin coroutines flow

--- src/main/kotlin/health/tracker/controller/HealthRecordController.kt
+++ src/main/kotlin/health/tracker/controller/HealthRecordController.kt
@@ -2,14 +2,15 @@ package health.tracker.controller
 
 import health.tracker.model.HealthRecord
 import health.tracker.persistence.HealthRecordRepository
-import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.flow.*
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Mono
 
 @RestController
 class HealthRecordController(private val healthRecordRepository: HealthRecordRepository) {
 
     @PostMapping("/health/{profileId}/record")
+    @ResponseStatus(HttpStatus.CREATED)
     suspend fun storeHealthRecord(@PathVariable profileId: Long, @RequestBody record: HealthRecord): HealthRecord {
         return healthRecordRepository.save(
             HealthRecord(
@@ -18,25 +19,32 @@ class HealthRecordController(private val healthRecordRepository: HealthRecordRep
         )
     }
 
+    @DeleteMapping("/health/{profileId}/records")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    suspend fun deleteRecords(@PathVariable profileId: Long) {
+        healthRecordRepository.deleteAll()
+    }
+
+    @GetMapping("/health/{profileId}/records")
+    suspend fun records(@PathVariable profileId: Long): Flow<HealthRecord> =
+        healthRecordRepository.findByProfileId(profileId).onEach { println(it) }
+
+
     @GetMapping("/health/{profileId}/avg")
     suspend fun avg(@PathVariable profileId: Long): AverageHealthStatus =
-        healthRecordRepository.findByProfileId(profileId)
-            .reduce(
-                AverageHealthStatus(0, 0.0, 0.0, 0.0)
-            ) { s, r ->
-                AverageHealthStatus(
-                    s.cnt + 1,
-                    s.temperature + r.temperature,
-                    s.bloodPressure + r.bloodPressure,
-                    s.heartRate + r.heartRate
-                )
-            }.map { s ->
-                AverageHealthStatus(
-                    s.cnt,
-                    if (s.cnt != 0) s.temperature / s.cnt else 0.0,
-                    if (s.cnt != 0) s.bloodPressure / s.cnt else 0.0,
-                    if (s.cnt != 0) s.heartRate / s.cnt else 0.0
-                )
+        healthRecordRepository
+            .findByProfileId(profileId)
+            .fold(AverageHealthStatus(0, 0.0, 0.0, 0.0)) { acc, healthRecord ->
+                acc.cnt++
+                acc.temperature += healthRecord.temperature
+                acc.bloodPressure += healthRecord.bloodPressure
+                acc.heartRate += healthRecord.heartRate
+                acc
+            }.let {
+                it.bloodPressure /= it.cnt
+                it.temperature /= it.cnt
+                it.heartRate /= it.cnt
+                it
             }
 }
 
